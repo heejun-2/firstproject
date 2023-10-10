05c121356a0efa94679bfe47d64f3affb5effd41
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-10-10 18:09:52 +0900

    ArticleController

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -32,15 +32,14 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-//        System.out.println(form.toString());
         log.info(form.toString());
+
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-//        System.out.println(article.toString());
         log.info(article.toString());
+
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-//        System.out.println(saved.toString());
         log.info(saved.toString());
 
         return "redirect:/articles/" + saved.getId();
